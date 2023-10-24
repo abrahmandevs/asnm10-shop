9b79300e86a231d7a67148f968907f8f362aa0a1
arrahmna-dev <dra390@gmail.com>
2023-10-25 02:19:21 +0600

    add and update swiper

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,12 @@
-<script setup></script>
+<script setup>
+	import SwiperSlide from "./SwiperSlider.vue"
+</script>
 <template>
 	<div>HOme page</div>
+
+	<section class="px-4 sm:px-12 py-8 sm:py-16">
+		<SwiperSlide />
+	</section>
 </template>
 
 <style lang="scss" scoped></style>
